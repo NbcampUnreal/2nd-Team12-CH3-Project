eaf912980efbcade79de0272b8ecb265744bcad9
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-02-19 01:29:55 +0900

    [update] 입력 바인딩 및 입력 기능 구현

--- SPT/Source/SPT/Characters/SPTPlayerCharacter.h
+++ SPT/Source/SPT/Characters/SPTPlayerCharacter.h
@@ -9,6 +9,8 @@
 class USpringArmComponent;
 class UCameraComponent;
 
+struct FInputActionValue;
+
 UCLASS()
 class SPT_API ASPTPlayerCharacter : public ACharacter
 {
@@ -25,6 +27,19 @@ public:
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	UFUNCTION()
+	void Move(const FInputActionValue& value);
+	UFUNCTION()
+	void Look(const FInputActionValue& value);
+	UFUNCTION()
+	void StartJump(const FInputActionValue& value);
+	UFUNCTION()
+	void StopJump(const FInputActionValue& value);
+	UFUNCTION()
+	void StartSprint(const FInputActionValue& value);
+	UFUNCTION()
+	void StopSprint(const FInputActionValue& value);
 
 private:
 	// 카메라 관련 컴포넌트
@@ -32,4 +47,14 @@ private:
 	TObjectPtr<USpringArmComponent> SpringArmComp;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = true))
 	TObjectPtr<UCameraComponent> CameraComp;
+
+	// 기본 걷기 속도
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = true))
+	float NormalSpeed;
+	// 기본 걷기 속도 대비 몇 배로 빠르게 달릴지 결정
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = true))
+	float SprintSpeedMultiplier;
+	// 실제 달리기 속도
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = true))
+	float SprintSpeed;
 };
--- SPT/Source/SPT/Controllers/SPTPlayerController.h
+++ SPT/Source/SPT/Controllers/SPTPlayerController.h
@@ -6,12 +6,28 @@
 #include "GameFramework/PlayerController.h"
 #include "SPTPlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class SPT_API ASPTPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	ASPTPlayerController();
+
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputMappingContext> IMC;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> JumpAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> SprintAction;
 };
